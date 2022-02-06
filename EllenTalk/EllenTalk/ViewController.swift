a10a17fd0235c3ed5989511fcc21d4007beae1e5
ellenJ <jcrescent61@gmail.com>
2022-02-06 16:15:45 +0900

    first commit

---
+++ EllenTalk/EllenTalk/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EllenTalk
+//
+//  Created by Ellen on 2022/02/06.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
