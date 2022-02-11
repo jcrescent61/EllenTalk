9b51dba41f9f28e96b4872949b97d9bad902da3e
ellenJ <jcrescent61@gmail.com>
2022-02-11 09:55:59 +0900

    refactor: SignUpContentsData 열거형 SignUpViewController로 위치 이동

--- EllenTalk/EllenTalk/Scene/SignUp/SignUpViewController.swift
+++ EllenTalk/EllenTalk/Scene/SignUp/SignUpViewController.swift
@@ -7,8 +7,30 @@
 
 import UIKit
 
+
+protocol SignUpViewable: UIView {}
+
+enum SignUpContentsData {
+    case email
+    case password
+    case passwordCheck
+    
+    func returnView() -> SignUpViewable {
+        switch self {
+        case .email:
+            return SignUpContentsView(SignUpConponent(title: "이메일", placeHolder: "ex)jcrescent61@google.com"))
+        case .password:
+            return SignUpContentsView(SignUpConponent(title: "비밀번호", placeHolder: "대,소,특수문자 최소 1회 포함"))
+        case .passwordCheck:
+            return SignUpContentsView(SignUpConponent(title: "비밀번호 확인", placeHolder: ""))
+        }
+    }
+}
+
 class SignUpViewController: UIViewController {
-    private lazy var signUpView: SignUpView = SignUpView(frame: view.bounds)
+    private lazy var signUpView: SignUpView = SignUpView([SignUpContentsData.email.returnView(),
+                                                          SignUpContentsData.password.returnView(),
+                                                          SignUpContentsData.passwordCheck.returnView()])
     
     override func viewDidLoad() {
         super.viewDidLoad()
