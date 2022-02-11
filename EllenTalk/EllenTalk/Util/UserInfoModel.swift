47d56f14bdb48b50726bf4e64b339742c1c561ea
ellenJ <jcrescent61@gmail.com>
2022-02-11 09:59:18 +0900

    feat: 모든 모델을 Model().세부모델로 접근하기 위한 Model타입 생성

---
+++ EllenTalk/EllenTalk/Util/UserInfoModel.swift
@@ -0,0 +1,24 @@
+//
+//  UserInfo.swift
+//  EllenTalk
+//
+//  Created by Ellen on 2022/02/07.
+//
+
+import Foundation
+
+struct Model { }
+
+extension Model {
+    struct UserInfo {
+        private let email: String
+        private let password: String
+        private let passwordCheck: String
+        
+        init(email: String, password: String, passwordCheck: String) {
+            self.email = email
+            self.password = password
+            self.passwordCheck = passwordCheck
+        }
+    }
+}
